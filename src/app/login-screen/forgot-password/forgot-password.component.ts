0eb4d76b74847a63f5a3fbe5be60bda9f64c7616
Ali Rashid <Ali.Rashid20@Outlook.com>
2022-07-30 23:03:25 +0500

    Second Commit

--- src/app/login-screen/forgot-password/forgot-password.component.ts
+++ src/app/login-screen/forgot-password/forgot-password.component.ts
@@ -12,4 +12,7 @@ export class ForgotPasswordComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onActivationLink(){
+    
+  }
 }
--- src/app/login-screen/new-user/new-user.component.ts
+++ src/app/login-screen/new-user/new-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-user',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
   }
 
+  onRegister(){
+    
+  }
 }
